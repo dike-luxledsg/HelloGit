5b21ae1ca0b5ccf758a754c34a403a22b4e79bdf
DIKE <dike@luxledsg.com>
2013-06-10 20:19:54 +0800

    Revert "sdf"
    This reverts commit cf41b68b78995fffca986e709dec84b7407ea479.

--- EditFile/edit_file.php
+++
@@ -1,8 +0,0 @@
-<?php
-	$edit = $_POST['edit'];
-	$file_name = $_POST['file_name'];
-	$file = fopen($file_name, 'w');
-	fwrite($file, $edit);
-	fclose($file);
-	echo "Content save successful<p><a href='index.php'>Click here to continue</a>";
-?>
